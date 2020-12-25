676a3e7078bc7291627310a5a13a82a42137f4fc
cyp0633 <cyp0633@gmail.com>
2020-12-25 21:35:54 +0800

    不知道添加了什么

--- main.cpp
+++ main.cpp
@@ -2,20 +2,18 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int bookCount;
 class Book
 {
 private:
     bool borrowed; //是否被借走了
 public:
-    Book()
+    void setBook(const int bookCount)
     {
         string title;            //题名
         bool serial;             //是否为连续出版物(ISSN)
         string num;              //ISBN/ISSN
         string author;           //作者
         string cat1, cat2, cat3; //三级分类
-        bookCount++;
         cout << "您正在录入第" << bookCount << "本图书的信息。\n";
         cout << "题目: ";
         cin >> title;
@@ -44,18 +42,47 @@ public:
             return false;
         }
         borrowed = true;
+        cout << "成功借书！";
         return true;
     }
     bool returnBook() //还书
     {
         if (!borrowed)
         {
-            cout << "这本书并没有被借走";
+            cout << "这本书并没有被借走，你是想再送给我们一本嘛？";
             return false;
         }
         borrowed = false;
+        cout << "成功还书！";
         return true;
     }
+};
+class repo //书库
+{
+private:
+    Book bookList[10000]; //暂时使用数组实现，试试啥时候能用链表
+    int bookCount = 0;
+
+public:
+    repo() {}
+    void addBook() //增加图书功能
+    {
+        bookList[bookCount].setBook(bookCount);
+        bookCount++;
+    }
+    void modBook(int num) //修改图书功能
+    {
+        bookList[bookCount].setBook(bookCount);
+    }
+};
+class account
+{
+};
+class student : public account
+{
+};
+class admin : public account
+{
 };
 int main()
 {
