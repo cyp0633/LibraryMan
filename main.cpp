7ed4d4f0adcb40ab14ea2343f2628c261be85f53
cyp0633 <cyp0633@gmail.com>
2020-12-29 14:48:51 +0800

    加入借书功能

--- main.cpp
+++ main.cpp
@@ -3,9 +3,16 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <list>
 #include <iomanip>
 using namespace std;
 bool kmp(string a, string b);
+struct logRecord //在馆记录
+{
+    int time; //时间
+    int id;   //人员ID
+    logRecord(int t, int i) : time(t), id(i) {}
+};
 class Book
 {
 private:
@@ -68,9 +75,12 @@ public:
 class repo //书库
 {
 private:
-    vector<Book> bookList; //暂时使用数组实现，试试啥时候能用链表
-
+    vector<Book> bookList;    //图书列表
+    vector<logRecord> logRec; //
 public:
+    void borrow(int target)
+    {
+    }
     void add() //增加图书功能
     {
         bookList.push_back(bookList.size() + 1);
@@ -249,9 +259,52 @@ public:
             }
         }
     }
+    friend class account;
 };
 class account
 {
+private:
+    int username;           //用户名
+    int password;           //密码
+    list<int> borrowedBook; //借了的书，使用链表存储，好删除
+public:
+    void borrowBook(repo &r)
+    {
+        cout << "您想怎样找到您想要的书？\n1.题名 | 2.ISBN/ISSN | 3.作者 | 4.分类号\n对于分类号，您可以使用任一级分类查找；作者无法模糊查找，必须匹配名字；题名和ISBN/ISSN可以模糊匹配，您只需要输入一部分。\n请输入您想查找的类型前的数字：";
+        int type, bookNum;
+        string target;
+        cin >> type;
+        switch (type)
+        {
+        case 1:
+            cout << "请输入您想查找的题名:";
+            cin >> target;
+            bookNum = r.searchTitle(target, 1);
+
+            break;
+        case 3:
+            cout << "请输入您想查找的作者:";
+            cin >> target;
+            bookNum = r.searchAuthor(target, 1);
+            break;
+        case 2:
+            cout << "请输入您想查找的ISBN/ISSN，记得输入中间的连字符:";
+            cin >> target;
+            bookNum = r.searchISBN(target, 1);
+            break;
+        case 4:
+            cout << "请输入您感兴趣的分类:";
+            cin >> target;
+            bookNum = r.searchCategory(target, 1);
+        }
+
+        if (r.bookList[bookNum].borrow()) //书被成功借走，会返回true，这样才会计入借走的列表
+        {
+            borrowedBook.push_back(bookNum);
+        }
+        return;
+    }
+    void searchBook(repo &r);
 };
 class student : public account
 {
@@ -261,6 +314,9 @@ class admin : public account
 };
 int main()
 {
+    repo library;
+    vector<student> studentList;
+    vector<admin> adminList;
     return 0;
 }
 bool kmp(string a, string b) //KMP是一种字符串匹配算法，可以实现b部分匹配a的查找。
